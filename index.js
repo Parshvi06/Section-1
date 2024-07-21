3050856a8dae165b969d0851881c8e1fcd99f37a
Parshvi06 <true.living1106@gmail.com>
2024-07-21 20:33:35 +0530

    javascriptStart

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log('Hello JavaScript');
+console.log('nice')
+
+num = 6373;
+myname =  'Parshvi';
+console.log(myname)
+console.log(typeof num)
+console.log(typeof myname) 
+console.log(typeof myname)
+console.log(1 == '1')
+console.log(1 === '1')
+
